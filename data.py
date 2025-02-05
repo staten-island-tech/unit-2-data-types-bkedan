e618263eccd60b9f8f977ca76633b0c7f02602d0
bkedan <EThany49@nycstudents.net>
2025-02-05 10:14:35 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+
+""" x = "this is a thing" # [this, is, a, thing]
+y= x.split( )
+z = y[3]
+print(y)
+print(z) """
+
